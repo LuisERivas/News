d7e30c19de7bba78317497da742c37825149b276
MCP <lucho.rivas.villacorta@gmail.com>
2019-09-09 12:33:23 -0700

    completed infrastructure for secondary article 1-4

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import MainAdvert from '../MainAdvertisment'
 import LatestNewsCluster from '../latestNews'
+import SecondaryNewsCluster from '../SecondaryNewsCluster'
 
 class Home extends Component{
     render(){
@@ -10,6 +11,7 @@ class Home extends Component{
                 <MainAdvert/>
                 <hr className='container'></hr>
                 <LatestNewsCluster/>
+                <SecondaryNewsCluster/>
             </div>
         )
     }
